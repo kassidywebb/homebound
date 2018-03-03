c9965277d3e313bb824ef1b78c546bb349f97bb4
Kassidy Webb <ktwebb@crimson.ua.edu>
2018-03-03 11:22:18 -0500

    added more pages

--- homeboundapp/lib/main.dart
+++ homeboundapp/lib/main.dart
@@ -34,6 +34,7 @@ class ButtonState extends State<Button> {
               backgroundColor: Colors.blueAccent[400]
             ),
             body: new Container(
+              decoration: new BoxDecoration(color: Colors.white),
               child: new Center(
                 child: new Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -68,6 +69,14 @@ class ButtonState extends State<Button> {
                     ),
                     //new Text(result)
 
+                    new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                    new Padding(padding: new EdgeInsets.all(10.0)),
+                    new RaisedButton(
+                        child: new Text("Submit", style: new TextStyle(color: Colors.white)),
+                        color: Colors.blueAccent[400],
+                        onPressed: submitPressed
+                    ),
+
                   ]
                 )
               )
@@ -77,6 +86,7 @@ class ButtonState extends State<Button> {
       ),
     );
   }
+
 
   void registerPressed() {
     Navigator.of(context).push(
@@ -90,6 +100,7 @@ class ButtonState extends State<Button> {
                   backgroundColor: Colors.blueAccent[400]
               ),
               body: new Container(
+                  decoration: new BoxDecoration(color: Colors.white),
                   child: new Center(
                       child: new Column(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -112,6 +123,21 @@ class ButtonState extends State<Button> {
                             new TextField(
                                 decoration: new InputDecoration(
                                     hintText: "Last Name"
+                                ),
+
+                                onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
+                                  setState(() {
+                                    result = loginstr;
+                                  });
+                                controller.text = "";
+                              },
+                             controller: controller */
+                            ),
+                            //new Text(result)
+
+                            new TextField(
+                                decoration: new InputDecoration(
+                                    hintText: "School"
                                 ),
 
                                 onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
@@ -153,6 +179,14 @@ class ButtonState extends State<Button> {
                              controller: controller */
                             ),
                             //new Text(result)
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Submit", style: new TextStyle(color: Colors.white)),
+                                color: Colors.blueAccent[400],
+                                onPressed: submitPressed
+                            ),
                           ]
                       )
                   )
@@ -164,6 +198,218 @@ class ButtonState extends State<Button> {
   }
 
 
+  void submitPressed() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text('Profile'),
+                  backgroundColor: Colors.blueAccent[400]
+              ),
+              body: new Container(
+                  decoration: new BoxDecoration(color: Colors.white),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("First Name Would Go Here"),
+                            new Text("Last Name Would Go Here"),
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Find Ride", style: new TextStyle(color: Colors.white)),
+                                color: Colors.blueAccent[400],
+                                onPressed: findRidePressed
+                            ),
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Give Ride", style: new TextStyle(color: Colors.white)),
+                                color: Colors.blueAccent[400],
+                                onPressed: giveRidePressed
+                            ),
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  void findRidePressed() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text('Find Ride'),
+                  backgroundColor: Colors.blueAccent[400]
+              ),
+              body: new Container(
+                  decoration: new BoxDecoration(color: Colors.white),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("Rides show here"),
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+
+  void giveRidePressed() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text('Give Ride'),
+                  backgroundColor: Colors.blueAccent[400]
+              ),
+              body: new Container(
+                  decoration: new BoxDecoration(color: Colors.white),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new TextField(
+                                decoration: new InputDecoration(
+                                    hintText: "Where are you going? (City, State)"
+                                ),
+
+                                onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
+                                  setState(() {
+                                    result = loginstr;
+                                  });
+                                controller.text = "";
+                              },
+                             controller: controller */
+                            ),
+                            //new Text(result)
+
+                            new TextField(
+                                decoration: new InputDecoration(
+                                    hintText: "How many people can you take?"
+                                ),
+
+                                onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
+                                  setState(() {
+                                    result = loginstr;
+                                  });
+                                controller.text = "";
+                              },
+                             controller: controller */
+                            ),
+                            //new Text(result)
+
+                            new TextField(
+                                decoration: new InputDecoration(
+                                    hintText: "When are you leaving?"
+                                ),
+
+                                onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
+                                  setState(() {
+                                    result = loginstr;
+                                  });
+                                controller.text = "";
+                              },
+                             controller: controller */
+                            ),
+                            //new Text(result)
+
+                            new TextField(
+                                decoration: new InputDecoration(
+                                    hintText: "What color and type of car will you be driving?"
+                                ),
+
+                                onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
+                                  setState(() {
+                                    result = loginstr;
+                                  });
+                                controller.text = "";
+                              },
+                             controller: controller */
+                            ),
+                            //new Text(result)
+
+                            new TextField(
+                                decoration: new InputDecoration(
+                                    hintText: "Where should everyone meet you?"
+                                ),
+
+                                onSubmitted: null /* {  //FIX ME I NEED TO GO TO EXCEL
+                                  setState(() {
+                                    result = loginstr;
+                                  });
+                                controller.text = "";
+                              },
+                             controller: controller */
+                            ),
+                            //new Text(result)
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Submit Ride", style: new TextStyle(color: Colors.white)),
+                                color: Colors.blueAccent[400],
+                                onPressed: submitRidePressed
+                            ),
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+
+  void submitRidePressed() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text('Your Ride Info'),
+                  backgroundColor: Colors.blueAccent[400]
+              ),
+              body: new Container(
+                  decoration: new BoxDecoration(color: Colors.white),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("Ride shows here"),
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -172,6 +418,7 @@ class ButtonState extends State<Button> {
           backgroundColor: Colors.blueAccent[400]
       ),
       body: new Container(
+        decoration: new BoxDecoration(color: Colors.white),
         child: new Center(
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
