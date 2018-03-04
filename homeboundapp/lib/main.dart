8a9333e938e86162f744165ce63e3a9ac89b67e6
Kassidy Webb <ktwebb@crimson.ua.edu>
2018-03-04 06:53:41 -0500

    Made ride pages

--- homeboundapp/lib/main.dart
+++ homeboundapp/lib/main.dart
@@ -223,8 +223,9 @@ class ButtonState extends State<Button> {
                       child: new Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            new Text("First Name Would Go Here"),
-                            new Text("Last Name Would Go Here"),
+                            new Image.asset('images/propic.jpg'),
+                            new Text("Kedron Abbott", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
 
                             new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
                             new Padding(padding: new EdgeInsets.all(10.0)),
@@ -275,6 +276,22 @@ class ButtonState extends State<Button> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             new Text("Rides show here", style: new TextStyle(color: Colors.white)),
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Tallahassee, FL"),
+                                color: Colors.white,
+                                onPressed: tallahasseePressed
+                            ),
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Tuscaloosa, AL"),
+                                color: Colors.white,
+                                onPressed: tuscaloosaPressed
+                            ),
                           ]
                       )
                   )
@@ -434,7 +451,208 @@ class ButtonState extends State<Button> {
                       child: new Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            new Text("Ride shows here"),
+                            new Text("Going to Tallahassee, FL", style: new TextStyle(color: Colors.white)),
+                            new Text("Leaving at 9:30am, March 9th", style: new TextStyle(color: Colors.white)),
+                            new Text("Current people in car include:", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/sbobpropic.jpg'),
+                            new Text("Spongebob Squarepants", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/propic.jpg'),
+                            new Text("Kedron Abbott", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  void tallahasseePressed() {
+    Navigator.of(context).push(
+
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text("Going to Tallahassee, FL", style: new TextStyle(color: Colors.black)),
+                  backgroundColor: Colors.white
+              ),
+              body: new Container(
+                  decoration: const BoxDecoration(
+                    //color: Colors.white
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: const AssetImage("images/uabckgrd.jpg"),
+                    ),
+                  ),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("Current people in car:", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/sbobpropic.jpg'),
+                            new Text("Spongebob Squarepants", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
+                            new Text("Leaving at 10:30am, March 10th", style: new TextStyle(color: Colors.white)),
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Join Car"),
+                                color: Colors.white,
+                                onPressed: joinCarTallahasseePressed
+                            ),
+
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  void tuscaloosaPressed() {
+    Navigator.of(context).push(
+
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text("Going to Tuscaloosa, AL", style: new TextStyle(color: Colors.black)),
+                  backgroundColor: Colors.white
+              ),
+              body: new Container(
+                  decoration: const BoxDecoration(
+                    //color: Colors.white
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: const AssetImage("images/uabckgrd.jpg"),
+                    ),
+                  ),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("Current people in car:", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/sbobpropic.jpg'),
+                            new Text("Spongebob Squarepants", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
+                            new Text("Leaving at 9:30am, March 9th", style: new TextStyle(color: Colors.white)),
+
+                            new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+                            new Padding(padding: new EdgeInsets.all(10.0)),
+                            new RaisedButton(
+                                child: new Text("Join Car"),
+                                color: Colors.white,
+                                onPressed: joinCarTuscaloosaPressed
+                            ),
+
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  void joinCarTallahasseePressed() {
+    Navigator.of(context).push(
+
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text("Tallahassee", style: new TextStyle(color: Colors.black)),
+                  backgroundColor: Colors.white
+              ),
+              body: new Container(
+                  decoration: const BoxDecoration(
+                    //color: Colors.white
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: const AssetImage("images/uabckgrd.jpg"),
+                    ),
+                  ),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("Congrats! Have a fun trip!", style: new TextStyle(color: Colors.white)),
+
+                            new Text("You'll be going to Tallahassee with:", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/sbobpropic.jpg'),
+                            new Text("Spongebob Squarepants", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/propic.jpg'),
+                            new Text("Kedron Abbott", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
+                          ]
+                      )
+                  )
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  void joinCarTuscaloosaPressed() {
+    Navigator.of(context).push(
+
+      new MaterialPageRoute(
+        builder: (context) {
+          //what you want page to do
+
+          return new Scaffold(
+              appBar: new AppBar(
+                  title: new Text("Tuscaloosa", style: new TextStyle(color: Colors.black)),
+                  backgroundColor: Colors.white
+              ),
+              body: new Container(
+                  decoration: const BoxDecoration(
+                    //color: Colors.white
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: const AssetImage("images/uabckgrd.jpg"),
+                    ),
+                  ),
+                  child: new Center(
+                      child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Text("Congrats! Have a fun trip!", style: new TextStyle(color: Colors.white)),
+
+                            new Text("You'll be going to Tuscaloosa with:", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/sbobpropic.jpg'),
+                            new Text("Spongebob Squarepants", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
+                            new Image.asset('images/propic.jpg'),
+                            new Text("Kedron Abbott", style: new TextStyle(color: Colors.white)),
+                            new Text("The University of Alabama", style: new TextStyle(color: Colors.white)),
+
                           ]
                       )
                   )
